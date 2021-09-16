5870aefdaef86f835611ef2365ba0203c902015c
LAKKI <thakurluckysinghbrh@gmail.com>
2021-09-16 14:36:17 +0530

    added options to tag canvas

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 window.onload =
   function () {
+    const options = {
+      textColour: '#fff',
+      // freezeActive: true,
+      noSelect:true,
+      initial: [.2, .2],
+      minSpeed: .1,
+     };
   try {
-    TagCanvas.Start('skills');
+    TagCanvas.Start('skills','',options);
   } catch(e) {
     // something went wrong, hide the canvas container
     document.getElementById('skillsDiv').style.display = 'none';
